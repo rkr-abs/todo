ebcf27d6533c61d1817824359a6bad28a19c4d4a
RamKumar <rramkumar1306.com>
2022-12-19 11:21:46 +0530

    Refactored

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,20 +1,19 @@
 import AddTaskIcon from '@mui/icons-material/AddTask';
-import { Button } from '@mui/material';
+import { Box, Button } from '@mui/material';
 import { React } from 'react';
 import isInputEmpty from '../services/isInputEmpty';
 
-const AddTodo = ({ actions, state }) => {
-	const isEmpty = isInputEmpty(state);
+const props = ({ actions, state }) => ({
+	variant: 'contained',
+	disabled: isInputEmpty(state),
+	onClick: () => actions.addTodo(),
+});
 
-	return (
-		<Button
-			{ ...{ variant: 'contained',
-				disabled: isEmpty,
-				onClick: () => actions.addTodo() } }
-
-			sx={ { margin: '2px', padding: '12px' } }
-		> <AddTaskIcon sx={ { padding: '2px' } }/>
-			Add </Button>) ;
-};
+const AddTodo = (context) => <Box>
+	<Button
+		{ ...props(context) }
+		sx={ { margin: '2px', padding: '12px' } }
+	> <AddTaskIcon sx={ { padding: '2px' } }/>
+		Add </Button></Box> ;
 
 export default AddTodo;
